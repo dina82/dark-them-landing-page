947434d06da8e6b864794b15fa14b3f372ccbc2d
dina82 <dinaabdellatif94@gmail.com>
2021-11-05 23:06:47 +0200

    updates margin between sections

--- js/script.js
+++ js/script.js
@@ -9,5 +9,18 @@ function validateEmail(e) {
         document.querySelector('#errorMsg').style.display = "none"
 
     }
-
 }
+for (let i = 0; i < [1, 3, 5, 7].length; i++) {
+
+    console.log(i);
+    if (i == 3) {
+        // return 4
+    }
+}
+[1, 3, 5, 7].forEach(i => {
+    if (i == 3) {
+        return
+    }
+    console.log(i);
+
+});
